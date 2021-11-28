12d4ff8ff5d5ea37aada32bf76d909805d948ee3
H.Saki <44340370+saki-engineering@users.noreply.github.com>
2021-11-28 19:18:34 +0900

    section2-1のコードを追加

---
+++ chapter2/section1/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Comment struct {
+	CommentID int
+	ArticleID int
+	Message   string
+	CreatedAt time.Time
+}
+
+type Article struct {
+	ID          int
+	Title       string
+	Contents    string
+	UserName    string
+	NiceNum     int
+	CommentList []Comment
+	CreatedAt   time.Time
+}
+
+func main() {
+	comment1 := Comment{
+		CommentID: 1,
+		ArticleID: 1,
+		Message:   "test comment1",
+		CreatedAt: time.Now(),
+	}
+	comment2 := Comment{
+		CommentID: 2,
+		ArticleID: 1,
+		Message:   "second comment",
+		CreatedAt: time.Now(),
+	}
+	article := Article{
+		ID:          1,
+		Title:       "first article",
+		Contents:    "This is the test article.",
+		UserName:    "saki",
+		NiceNum:     1,
+		CommentList: []Comment{comment1, comment2},
+		CreatedAt:   time.Now(),
+	}
+
+	fmt.Printf("%+v\n", article)
+}
