f67ee1780ecf2283fe368be6c3cfb589913ba879
H.Saki <44340370+saki-engineering@users.noreply.github.com>
2021-12-14 23:45:31 +0900

    section6-2のコードを追加

---
+++ chapter6/section2/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/yourname/reponame/controllers"
+	"github.com/yourname/reponame/services"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	dbUser     = "docker"
+	dbPassword = "docker"
+	dbDatabase = "sampledb"
+	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+)
+
+func main() {
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("fail to connect DB")
+		return
+	}
+
+	ser := services.NewMyAppService(db)
+	con := controllers.NewMyAppController(ser)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
+
+	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
+
+	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
