31a24a73b4031ff00fb465ecd9f65d6542856363
H.Saki <44340370+saki-engineering@users.noreply.github.com>
2021-12-19 23:39:46 +0900

    section7-3のコードを追加

---
+++ chapter7/section3/controllers/comment_controller.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/yourname/reponame/apperrors"
+	"github.com/yourname/reponame/controllers/services"
+	"github.com/yourname/reponame/models"
+)
+
+type CommentController struct {
+	service services.CommentServicer
+}
+
+func NewCommentController(s services.CommentServicer) *CommentController {
+	return &CommentController{service: s}
+}
+
+// POST /comment のハンドラ
+func (c *CommentController) PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	var reqComment models.Comment
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		err = apperrors.ReqBodyDecodeFailed.Wrap(err, "bad request body")
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+
+	comment, err := c.service.PostCommentService(reqComment)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(comment)
+}
