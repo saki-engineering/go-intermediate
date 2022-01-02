205643a6f4a407fa59123ad7e13ac41440023214
H.Saki <44340370+saki-engineering@users.noreply.github.com>
2022-01-02 18:05:14 +0900

    section11-2のコードを追加

---
+++ chapter11/section2/api/middlewares/logging.go
@@ -0,0 +1,35 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+)
+
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		traceID := setTraceID()
+
+		// リクエスト情報をロギング
+		log.Printf("[%d]%s %s\n", traceID, req.RequestURI, req.Method)
+
+		rlw := NewResLoggingWriter(w)
+
+		next.ServeHTTP(rlw, req)
+
+		log.Printf("[%d]res: %d", traceID, rlw.code)
+	})
+}
---
+++ chapter11/section2/services/errors.go
@@ -0,0 +1,5 @@
+package services
+
+import "errors"
+
+var ErrNoData = errors.New("get 0 record from db.Query")
