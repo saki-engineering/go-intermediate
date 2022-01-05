5d2d744f6219d64b39d0177337b7976756796e2b
H.Saki <44340370+saki-engineering@users.noreply.github.com>
2022-01-05 19:20:29 +0900

    section12-2のコードを追加

---
+++ chapter12/section2/ver1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	id_token := `[yourIDToken]`
+
+	dataArray := strings.Split(id_token, ".")
+	header, payload, _ := dataArray[0], dataArray[1], dataArray[2]
+
+	// headerをbase64 decodeする
+	headerData, err := base64.RawURLEncoding.DecodeString(header)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	// payloadをbase64 decodeする
+	payloadData, err := base64.RawURLEncoding.DecodeString(payload)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	fmt.Println("header: ", string(headerData))
+	fmt.Println("payload: ", string(payloadData))
+}
---
+++ chapter12/section2/ver2/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"math/big"
+	"strings"
+)
+
+func main() {
+	id_token := `[yourIDToken]`
+
+	dataArray := strings.Split(id_token, ".")
+	header, payload, sig := dataArray[0], dataArray[1], dataArray[2]
+
+	// headerをbase64 decodeする
+	headerData, err := base64.RawURLEncoding.DecodeString(header)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	// payloadをbase64 decodeする
+	payloadData, err := base64.RawURLEncoding.DecodeString(payload)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	// 公開鍵を使えるようにする
+	N := "nnnnnnn"
+	E := "eeeeeee"
+
+	dn, _ := base64.RawURLEncoding.DecodeString(N)
+	de, _ := base64.RawURLEncoding.DecodeString(E)
+
+	pk := &rsa.PublicKey{
+		N: new(big.Int).SetBytes(dn),
+		E: int(new(big.Int).SetBytes(de).Int64()),
+	}
+
+	// 検証するデータ
+	message := sha256.Sum256([]byte(header + "." + payload))
+
+	// 署名をbase64 decodeする
+	sigData, err := base64.RawURLEncoding.DecodeString(sig)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	if err := rsa.VerifyPKCS1v15(pk, crypto.SHA256, message[:], sigData); err != nil {
+		fmt.Println("invalid token")
+	} else {
+		fmt.Println("valid token")
+		fmt.Println("header: ", string(headerData))
+		fmt.Println("payload: ", string(payloadData))
+	}
+}
